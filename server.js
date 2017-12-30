afa719ce8b78b6d9db1b20abf6e4c47d5da42238
Ilkka Oksanen <iao@iki.fi>
2017-12-30 13:53:33 +0100

    Show where the server is running

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const fs = require('fs');
 const Koa = require('koa');
 const send = require('koa-send');
 
+const PORT = 3000;
+
 const app = new Koa();
 
 app.use(async (ctx) => {
   await send(ctx, ctx.path, { index: 'index.html', root: __dirname + '/platform/player' });
 });
 
-app.listen(3000);
+app.listen(PORT);
+
+console.log(`Server started: http://localhost:${PORT}/`);
