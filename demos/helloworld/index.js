77ad39effe5d662af61559aa24e68a18122f1346
Ilkka Oksanen <iao@iki.fi>
2018-01-01 14:20:55 +0100

    Add hello world demo

---
+++ demos/helloworld/index.js
@@ -0,0 +1,21 @@
+import { setPixel } from 'engine';
+
+let currentColor = 0;
+
+function start() {
+}
+
+function draw() {
+  for (let i = 0; i < 360; i++) {
+    setPixel(i + 130, i, currentColor, 255, currentColor);
+    setPixel(i + 130, 360 - i, 255, currentColor, currentColor);
+  }
+
+  currentColor++;
+
+  if (currentColor === 255) {
+    currentColor = 0;
+  }
+}
+
+export { start, draw };
