8c6757fa44b6c440214166a2555c85a2b75c364b
Ilkka Oksanen <iao@iki.fi>
2018-02-18 19:21:19 +0100

    Fix paths

--- server/webpackConfig.js
+++ server/webpackConfig.js
@@ -5,13 +5,16 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
 module.exports = function generate(options) {
   return {
     mode: options.mode,
-    entry: path.resolve(__dirname, '..', 'main'),
+    entry: path.resolve(__dirname, '../main'),
     output: {
-      path: path.resolve(__dirname, '..', 'dist'),
+      path: path.resolve(__dirname, '../dist'),
       filename: options.mode === 'production' ? 'main-[chunkhash].js' : 'main.js'
     },
     resolve: {
-      modules: [path.resolve(__dirname, '..')]
+      modules: [
+        path.resolve(__dirname, '..'),
+        path.resolve(__dirname, '../node_modules')
+      ]
     },
     stats: {
       maxModules: Infinity,
